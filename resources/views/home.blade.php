288f2f9f8d938957b09fbc1edb885af5b581b9cd
JOSH DANE MONCADA LABISTRE <2300337@cc.usls.edu>
2026-01-19 12:18:57 +0800

    init home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,7 @@
 
 @section('title', 'Home')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Welcome to my Portfolio</h1>
-</body>
-</html>
+@section('content')
+<h1>Welcome to my Portfolio</h1>
+<p>This is my personal website.</p>
+@endsection
